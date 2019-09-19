4c44eeba2fcf2f7a31eaa818ac197b30bc40dd50
Thierry Gerbeau <thierry.gerbeau@gmail.com>
2019-09-19 06:43:27 +0200

    first API test request

--- tests/example.js
+++ tests/example.js
@@ -5,6 +5,9 @@ const config = require ('../lib/config')
 const click = require ('../lib/helpers').click
 const typeText = require ('../lib/helpers').typeText
 const loadUrl = require ('../lib/helpers').loadUrl
+const assert = require('assert')
+const request = require ('request')
+
 
 describe ('My first puppeteer', () => {
     let browser
@@ -29,26 +32,52 @@ describe ('My first puppeteer', () => {
         await browser.close()
      })
 
-    it('My first test step', async () => {
-        await loadUrl(page, config.baseUrl)
-        await page.waitForSelector('#nav-search')
+    function APItesting (requestToPlay, timeoutThreshold) {
+        
+        return new Promise((resolve, reject) => {
+            request (requestToPlay, function (error,response,body){
+                if (! error) {
+                    data = JSON.parse(body)
+                    let obj = { codePostal: data[0].codePostal, 
+                        codeCommune: data[0].codeCommune,
+                        nomCommune: data[0].nomCommune,
+                        libelleAcheminement: data[0].libelleAcheminement};
 
-        const url = await page.url()
-        const title = await page.title()
+                    if (obj.codePostal === '94300'
+                    && obj.codeCommune === '94080' 
+                    && obj.nomCommune === 'Vincennes'
+                    && obj.libelleAcheminement === 'VINCENNES'
+                    ) {
+                        resolve("Success");
+                    }
+                    else {
+                        reject("Échec dans les valeurs retournées par la requete.");
+                    }    
+                } else {
+                        reject("request fail");
+                }
+                
+            });
+            setTimeout (function() {
+                reject('Request Timeout');
+            },timeoutThreshold);
+        });
+    }
 
-        expect(url).to.contain("dev")
-        expect(title).to.contain("Community")
+
+    async function asyncCall () {
+        var requestToPlay1 =`https://apicarto.in.fr/api/codes-postaux/communes/94300` 
+        //var requestToPlay2 =`https://apiarto.ign.fr/api/codes-postaux/communes/93600` 
+        var promise1 = await APItesting(requestToPlay1,100);
+        var promise2 = await APItesting(requestToPlay1,100);
+
+   
+        Promise.all([promise1,promise2]).then(function(values){
+            console.log(values);
+        }); 
+    }
+    
+    it('asyncCall',async () => {
+        asyncCall ();
     })
-    it('click method',async () => {
-        await loadUrl(page, config.baseUrl)
-        await click (page, '#write-link') 
-        await page.waitForSelector('.registration-rainbow')                 
-    })
-
-    it('type_something_in_search_bar', async () => {
-        await loadUrl(page, config.baseUrl)
-        await typeText (page,"something great here", '#nav-search')
-        await page.keyboard.press('Enter')
-        await page.waitForSelector('#articles-list')
-    })  
-})
+});
